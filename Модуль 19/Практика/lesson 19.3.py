f2d2e72c4e4ba0c52c7a18cc534c5fd247513077
IgorZin <ziok2018@gmail.com>
2022-10-16 02:03:14 +0300

    Загрузка 16.10.22 Модуль 19 практика

--- Модуль 19/Практика/lesson 19.3.py
+++ Модуль 19/Практика/lesson 19.3.py
@@ -49,11 +49,65 @@ if task == 1:
     print('=' * 40)
     print('Задача 1')
 
+    family_member = dict()
+    human = input('Введите через пробел имя, фамилию, кол-во лет: ').split()
+    hobbi = input('Введите хобби через пробел: ').split()
+    scor_children = int(input('Введите сколько детей: '))
+
+    family_member['name'] = human[0]
+    family_member['surname'] = human[1]
+    family_member['hobbies'] = []
+    family_member['age'] = human[2]
+    family_member['children'] = {}
+    for hob in hobbi:
+        family_member['hobbies'].append(hob)
+    for child in range(scor_children):
+        name = input('Введите имя и возраст ребенка через пробел: ').split()
+        family_member['children'][name[0]] = name[1]
+    search = input('Введите имя ребенка для поиска: ')
+    if family_member.get('children').get(search):
+        print(family_member['surname'], search)
+    else:
+        print('Такого ребенка нет.')
+
 
 elif task == 2:
     # Задача 2
     print('=' * 40)
     print('Задача 2')
+
+
+    players_dict = {
+        1: {'name': 'Vanya', 'team': 'A', 'status': 'Rest'},
+        2: {'name': 'Lena', 'team': 'B', 'status': 'Training'},
+        3: {'name': 'Maxim', 'team': 'C', 'status': 'Travel'},
+        4: {'name': 'Egor', 'team': 'C', 'status': 'Rest'},
+        5: {'name': 'Andrei', 'team': 'A', 'status': 'Training'},
+        6: {'name': 'Sasha', 'team': 'A', 'status': 'Rest'},
+        7: {'name': 'Alina', 'team': 'B', 'status': 'Rest'},
+        8: {'name': 'Masha', 'team': 'C', 'status': 'Travel'}
+    }
+
+    result_2 = [
+        i_num['name']
+        for i_num in players_dict.values()
+        if i_num['team'] == 'A' and i_num['status'] == 'Rest'
+    ]
+    print(result_2)
+
+    result_2 = [
+        i_num['name']
+        for i_num in players_dict.values()
+        if i_num['team'] == 'B' and i_num['status'] == 'Training'
+    ]
+    print(result_2)
+
+    result_3 = [
+        i_num['name']
+        for i_num in players_dict.values()
+        if i_num['team'] == 'C' and i_num['status'] == 'Travel'
+    ]
+    print(result_3)
 
 
 elif task == 3:
