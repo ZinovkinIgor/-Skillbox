4f95f3f8789b6cea703c46c777e8e7049f402837
IgorZin <ziok2018@gmail.com>
2022-10-19 01:03:06 +0300

    Загрузка 18.10.22 Модуль 19 домашка

--- Модуль 19/Домашнее задание/task 6.py
+++ Модуль 19/Домашнее задание/task 6.py
@@ -17,3 +17,27 @@
 Введите слово: здравствуйте
 Синоним: Привет
 """
+
+scor = int(input('Введите количество пар слов: '))
+word_dict = dict()
+for num in range(1, scor + 1):
+    print(num, 'пара: ', end='')
+    couple_word = input().split(' - ')
+    word_dict[couple_word[0]] = couple_word[1]
+
+while True:
+    search_word = input('Введите слово: ')
+    if search_word == 'end':
+        break
+    if search_word in word_dict.keys():
+        print('Синоним:', word_dict[search_word])
+    elif search_word in word_dict.values():
+        for simb, value in word_dict.items():
+            if search_word == value:
+                print('Синоним:', simb)
+    else:
+        print('Такого слова в словаре нет.')
+
+
+
+
