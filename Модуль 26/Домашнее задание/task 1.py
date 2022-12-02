8f9c3423c85aee503e6cc69978f69c187bb43156
IgorZin <ziok2018@gmail.com>
2022-12-02 12:41:46 +0300

    Загрузка 02.12.22 Модуль 26 домашка

--- Модуль 26/Домашнее задание/task 1.py
+++ Модуль 26/Домашнее задание/task 1.py
@@ -7,7 +7,7 @@
 функция-генератор и генераторное выражение.
 """
 
-task = int(input('Выберите какую задачу выполнить (1-класс-итератор, 2-функция-генератор, 3-генераторное выражение)'))
+task = int(input('Выберите какую задачу выполнить (1-класс-итератор, 2-функция-генератор, 3-генераторное выражение) '))
 
 if task == 1:
     # Задача 1
@@ -17,19 +17,46 @@ if task == 1:
     class Square:
         def __init__(self, number):
             self.number = number
+            self.score = 0
 
+        def __iter__(self):
+            return self
 
+        def __next__(self):
+            while self.score < self.number:
+                self.score += 1
+                yield self.score ** 2
+
+    numb = Square(5000000)
+    for n in numb.__next__():
+        print(n)
 
 elif task == 2:
     # Задача 2
     print('=' * 40)
     print('2-функция-генератор')
 
+    def func_square(number):
+        i = 0
+        while i <= number:
+            yield i ** 2
+            i += 1
+
+
+    numb = func_square(5)
+    for n in numb:
+        print(n)
+
 
 elif task == 3:
     # Задача 3
     print('=' * 40)
     print('3-генераторное выражение')
+
+    square_number = (x ** 2 for x in range(1, 11))
+    for n in square_number:
+        print(n)
+
 
 else:
     print('Выберите задачу заново.')
